ff6855518b1ec0e0a602d4665fe36903bc083c50
laryssa150 <ferreiralaryssa623@gmail.com>
2025-06-04 10:58:19 -0300

    Add files via upload

---
+++ GPSFrancisco/frmPesquisarVoluntarios.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace GPSFrancisco
+{
+    partial class frmPesquisarVoluntarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPesquisarVoluntarios));
+            this.prtAtribuicoes = new System.Drawing.Printing.PrintDocument();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.gpbPesquisarPor = new System.Windows.Forms.GroupBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.rdbNome = new System.Windows.Forms.RadioButton();
+            this.rdbCodigo = new System.Windows.Forms.RadioButton();
+            this.ltbPesquisar = new System.Windows.Forms.ListBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnPesquisarVoluntarios = new System.Windows.Forms.Button();
+            this.gpbPesquisarPor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpbPesquisarPor
+            // 
+            this.gpbPesquisarPor.Controls.Add(this.txtDescricao);
+            this.gpbPesquisarPor.Controls.Add(this.lblDescricao);
+            this.gpbPesquisarPor.Controls.Add(this.rdbNome);
+            this.gpbPesquisarPor.Controls.Add(this.rdbCodigo);
+            this.gpbPesquisarPor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbPesquisarPor.Location = new System.Drawing.Point(12, 12);
+            this.gpbPesquisarPor.Name = "gpbPesquisarPor";
+            this.gpbPesquisarPor.Size = new System.Drawing.Size(517, 140);
+            this.gpbPesquisarPor.TabIndex = 11;
+            this.gpbPesquisarPor.TabStop = false;
+            this.gpbPesquisarPor.Text = "Pesquisar por";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Enabled = false;
+            this.txtDescricao.Location = new System.Drawing.Point(122, 76);
+            this.txtDescricao.MaxLength = 50;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(289, 26);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Location = new System.Drawing.Point(22, 77);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(80, 20);
+            this.lblDescricao.TabIndex = 2;
+            this.lblDescricao.Text = "Descrição";
+            // 
+            // rdbNome
+            // 
+            this.rdbNome.AutoSize = true;
+            this.rdbNome.Location = new System.Drawing.Point(215, 27);
+            this.rdbNome.Name = "rdbNome";
+            this.rdbNome.Size = new System.Drawing.Size(69, 24);
+            this.rdbNome.TabIndex = 2;
+            this.rdbNome.Text = "Nome";
+            this.rdbNome.UseVisualStyleBackColor = true;
+            this.rdbNome.CheckedChanged += new System.EventHandler(this.rdbNome_CheckedChanged);
+            // 
+            // rdbCodigo
+            // 
+            this.rdbCodigo.AutoSize = true;
+            this.rdbCodigo.Location = new System.Drawing.Point(122, 27);
+            this.rdbCodigo.Name = "rdbCodigo";
+            this.rdbCodigo.Size = new System.Drawing.Size(77, 24);
+            this.rdbCodigo.TabIndex = 1;
+            this.rdbCodigo.Text = "Código";
+            this.rdbCodigo.UseVisualStyleBackColor = true;
+            this.rdbCodigo.CheckedChanged += new System.EventHandler(this.rdbCodigo_CheckedChanged);
+            // 
+            // ltbPesquisar
+            // 
+            this.ltbPesquisar.Enabled = false;
+            this.ltbPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltbPesquisar.FormattingEnabled = true;
+            this.ltbPesquisar.ItemHeight = 20;
+            this.ltbPesquisar.Location = new System.Drawing.Point(12, 158);
+            this.ltbPesquisar.Name = "ltbPesquisar";
+            this.ltbPesquisar.Size = new System.Drawing.Size(518, 144);
+            this.ltbPesquisar.TabIndex = 12;
+            this.ltbPesquisar.SelectedIndexChanged += new System.EventHandler(this.ltbPesquisar_SelectedIndexChanged);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Enabled = false;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Image = ((System.Drawing.Image)(resources.GetObject("btnLimpar.Image")));
+            this.btnLimpar.Location = new System.Drawing.Point(536, 160);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(160, 140);
+            this.btnLimpar.TabIndex = 14;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnPesquisarVoluntarios
+            // 
+            this.btnPesquisarVoluntarios.Enabled = false;
+            this.btnPesquisarVoluntarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisarVoluntarios.Image = ((System.Drawing.Image)(resources.GetObject("btnPesquisarVoluntarios.Image")));
+            this.btnPesquisarVoluntarios.Location = new System.Drawing.Point(536, 12);
+            this.btnPesquisarVoluntarios.Name = "btnPesquisarVoluntarios";
+            this.btnPesquisarVoluntarios.Size = new System.Drawing.Size(160, 140);
+            this.btnPesquisarVoluntarios.TabIndex = 13;
+            this.btnPesquisarVoluntarios.Text = "&Pesquisar";
+            this.btnPesquisarVoluntarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnPesquisarVoluntarios.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnPesquisarVoluntarios.UseVisualStyleBackColor = true;
+            this.btnPesquisarVoluntarios.Click += new System.EventHandler(this.btnPesquisarVoluntarios_Click);
+            // 
+            // frmPesquisarVoluntarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 311);
+            this.Controls.Add(this.gpbPesquisarPor);
+            this.Controls.Add(this.ltbPesquisar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnPesquisarVoluntarios);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPesquisarVoluntarios";
+            this.Text = "GSFrancisco - Pesquisar Voluntários";
+            this.gpbPesquisarPor.ResumeLayout(false);
+            this.gpbPesquisarPor.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Drawing.Printing.PrintDocument prtAtribuicoes;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.GroupBox gpbPesquisarPor;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.RadioButton rdbNome;
+        private System.Windows.Forms.RadioButton rdbCodigo;
+        private System.Windows.Forms.ListBox ltbPesquisar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnPesquisarVoluntarios;
+    }
+}
---
+++ GPSFrancisco/frmPesquisarVoluntarios.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace GPSFrancisco
+{
+    public partial class frmPesquisarVoluntarios : Form
+    {
+        public frmPesquisarVoluntarios()
+        {
+            InitializeComponent();
+        }
+
+        private void ltbPesquisar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string nome = ltbPesquisar.SelectedItem.ToString();
+
+            frmGerenciarVoluntarios abrir = new frmGerenciarVoluntarios();
+            abrir.Show();
+            this.Hide();
+
+        }
+
+        private void btnPesquisarVoluntarios_Click(object sender, EventArgs e)
+        {
+            if (rdbCodigo.Checked == false && rdbNome.Checked == false)
+            {
+                MessageBox.Show("Favor selecionar um item");
+            }
+            else if(txtDescricao.Text.Equals(""))
+            {
+                MessageBox.Show("Favor inserir um valor","mensagem do sistema", MessageBoxButtons.OK, MessageBoxIcon.Error,MessageBoxDefaultButton.Button1);
+                txtDescricao.Focus();
+            }
+            else {
+                if (rdbCodigo.Checked)
+                {
+                    buscarVoluntariosporCodigo(Convert.ToInt32(txtDescricao.Text));
+                }
+                if (rdbNome.Checked)
+                {
+                    buscarVoluntariosporNome(txtDescricao.Text);
+                }
+            }
+           
+            
+
+        }
+
+        //busca por código
+
+        public void buscarVoluntariosporCodigo(int codVol)
+        {
+            MySqlCommand comm = new MySqlCommand();
+            comm.CommandText = "SELECT * FROM tbvoluntarios WHERE codvol = @codVol;";
+            comm.CommandType = CommandType.Text;
+
+            comm.Parameters.Clear();
+            comm.Parameters.Add("@codVol",MySqlDbType.Int32).Value = codVol;
+            comm.Connection = Conexao.obterConexao();
+
+
+            MySqlDataReader DR;
+            DR = comm.ExecuteReader();
+            DR.Read();
+
+            ltbPesquisar.Items.Clear();
+
+            ltbPesquisar.Items.Add(DR.GetInt32(0));
+            
+            Conexao.fecharConexao();
+
+
+        }
+
+        //busca por nome
+
+        public void buscarVoluntariosporNome(string nome)
+        {
+            MySqlCommand comm = new MySqlCommand();
+            comm.CommandText = "SELECT * FROM tbvoluntarios WHERE nome like '%" + nome + "%';";
+            comm.CommandType = CommandType.Text;
+
+            comm.Connection = Conexao.obterConexao();
+
+
+            MySqlDataReader DR;
+            DR = comm.ExecuteReader();
+
+           ltbPesquisar.Items.Clear();
+
+            while (DR.Read())
+            { 
+                ltbPesquisar.Items.Add(DR.GetString(1));
+            }
+            Conexao.fecharConexao();
+
+
+        }
+
+
+        private void rdbCodigo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbCodigo.Checked)
+            {
+                habilitarCampos();
+            }
+        }
+        //habilitar campos
+        public void habilitarCampos()
+        {
+            txtDescricao.Enabled = true;
+            ltbPesquisar.Enabled = true;
+            btnLimpar.Enabled = true;
+            btnPesquisarVoluntarios.Enabled = true;
+            txtDescricao.Focus();
+        }
+
+        private void rdbNome_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbNome.Checked)
+            {
+                habilitarCampos();
+            }
+        }
+         //limpar campos
+         public void LimparCampos()
+        {
+            txtDescricao.Clear();
+            ltbPesquisar.Items.Clear();
+            rdbCodigo.Checked = false;
+            rdbNome.Checked = false;
+            txtDescricao.Focus();
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+    }
+}
