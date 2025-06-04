50fc572d4dba6836bb8fafa542f33ffaba4920fc
laryssa150 <ferreiralaryssa623@gmail.com>
2025-06-04 10:48:30 -0300

    Update dbgpFrancisco.sql

--- dbgpFrancisco.sql
+++ dbgpFrancisco.sql
@@ -41,35 +41,26 @@ foreign key(codAtr) references tbAtribuicoes(codAtr));
 insert into tbVoluntarios (nome,email,telCel,endereco,numero,cep,bairro,cidade,estado,codAtr,data,hora,status);
 values(nome,email,telCel,endereco,numero,cep,bairro,cidade,estado,codAtr,data,hora,status);
 
---insert into tbUsuarios(nome,senha)values('senac', 'senac');
+insert into tbUsuarios(nome,senha)values('senac', 'senac');
  
---select * from tbUsuarios;
+select * from tbUsuarios;
  
---select nome,senha from tbUsuarios where nome='senac' and senha='senac';
+select nome,senha from tbUsuarios where nome='senac' and senha='senac';
 
 
---update tbUsuarios set nome = 'senac', senha = '123456789123',where codUsu = 1;
+update tbUsuarios set nome = 'senac', senha = '123456789123',where codUsu = 1;
 
 -- pesquisa filtrada por nome
---select * from tbusua
---rios where nome like '%nome';
+select * from tbusuarios where nome like '%nome';
 
---select * from tbUsuarios where nome = 'senac';
+select * from tbUsuarios where nome = 'senac';
 
---delete from tbUsuarios where codUsu = '5';
+delete from tbUsuarios where codUsu = '5';
 
--- pesquisa filtrada por codigo
+--pesquisa filtrada por codigo
 
---select * from tbUsuarios where codUsu = codUsu;
+select * from tbUsuarios where codUsu = codUsu;
 
 
---create table tbVoluntarios(
---codUsu int not null auto_increment,
---nome varchar(30) not null,
---senha varchar(12) not null,
---primary key(codUsu),
---foreign key(codVol)
-)
-
--- SELECT * FROM `tbvoluntarios` WHERE codvol = codVol
---SELECT * FROM tbvoluntarios WHERE nome like '%nome%';
+SELECT * FROM `tbvoluntarios` WHERE codvol = codVol
+SELECT * FROM tbvoluntarios WHERE nome like '%nome%';
